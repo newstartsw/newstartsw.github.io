ca9f2ac281ec24f939f9f1fc179ae6e9d7a070b8
newstartsw <newstartsw@users.noreply.github.com>
2025-09-11 13:45:19 +0000

    deploy: newstartsw/hexo-action@9704fd7f0dcc176627b296d8aa1416342db2f417

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/04/hexo-action/","2025/09/03/hello-world/","2025/09/05/links-bot/"];function toRandomPost(){
+var posts=["2025/09/03/hello-world/","2025/09/04/hexo-action/","2025/09/05/links-bot/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
